707e154c8f0dcd929b5eeb534f4e3dfd0595ce1e
vitche <doker.bucket@gmail.com>
2020-10-14 00:06:47 +0300

    little +

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $contacts = User::where('id', '!=', auth()->id())->get();
+
+        return view('home', ['contacts' => $contacts]);
     }
 }
